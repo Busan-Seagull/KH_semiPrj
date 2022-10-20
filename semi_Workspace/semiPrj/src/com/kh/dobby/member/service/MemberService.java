255184c9e4d005092a4f383007fc278c8450ed68
gaesoony <gae.soo.ny@gmail.com>
2022-10-20 15:26:48 +0900

    이도경 회원가입 서버구현 중(회원가입 시 인설트 됨)

--- semi_Workspace/semiPrj/src/com/kh/dobby/member/service/MemberService.java
+++ semi_Workspace/semiPrj/src/com/kh/dobby/member/service/MemberService.java
@@ -5,6 +5,7 @@ import java.sql.Connection;
 import com.kh.dobby.common.JDBCTemplate;
 import com.kh.dobby.member.dao.MemberDao;
 import com.kh.dobby.member.vo.MemberVo;
+import com.kh.dobby.member.vo.RightVo;
 
 public class MemberService {
 
@@ -20,6 +21,27 @@ public class MemberService {
 
             return loginMember; 
         
+
+    }
+
+    public int join(MemberVo vo, RightVo rightvo) {
+        Connection conn=JDBCTemplate.getConnection();
+        MemberDao dao = new MemberDao();
+        int result=dao.insertJoin(conn, vo);
+        
+        int result2=1;
+        if(rightvo!=null){
+            result2=dao.insertJoinDobby(conn,rightvo);
+        }
+        if(result*result2==1) {
+            JDBCTemplate.commit(conn);
+        }else {
+            JDBCTemplate.rollback(conn);
+        }
+        
+        JDBCTemplate.close(conn);
+        
+        return result*result2;
 
     }
 
