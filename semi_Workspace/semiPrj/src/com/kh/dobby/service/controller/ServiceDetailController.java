0df83a5c6fa22c9063bd6584e9937d9f974ef632
DESKTOP-FAGA4A5\Ryan <ryangwooz@gmail.com>
2022-11-02 17:56:22 +0900

    자잘..하게..수정..!!

--- semi_Workspace/semiPrj/src/com/kh/dobby/service/controller/ServiceDetailController.java
+++ semi_Workspace/semiPrj/src/com/kh/dobby/service/controller/ServiceDetailController.java
@@ -8,10 +8,14 @@ import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 
+import com.kh.dobby.member.vo.MemberVo;
 import com.kh.dobby.review.vo.ReviewVo;
 import com.kh.dobby.service.service.ServiceService;
 import com.kh.dobby.service.temp.Service;
+import com.kh.dobby.service.temp.ZzimServiceTemp;
+import com.kh.dobby.service.temp.ZzimVoTemp;
 import com.kh.dobby.service.vo.CodeVo;
 import com.kh.dobby.service.vo.ServiceVo;
 
@@ -21,21 +25,28 @@ public class ServiceDetailController extends HttpServlet {
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
         
+        HttpSession s = req.getSession();
         // 데이터 꺼내기
-
+        MemberVo loginMember = (MemberVo)s.getAttribute("loginMember");
+        String uno = null;
+        if(loginMember != null) {
+        uno = loginMember.getUserNo();
+        }
         String sno = req.getParameter("sno");
 
         if (sno != null) {
             ServiceVo sv = new ServiceService().selectOne(sno);
             CodeVo cv = new ServiceService().changeCode(sv);
             List<ReviewVo> rvList = new Service().selectList(null, sno);
+            ZzimVoTemp zzim = new ZzimServiceTemp().checkZzim(sno, uno);
             
-            
+            System.out.println(zzim);
             System.out.println(rvList);
 
             req.setAttribute("cv", cv);
             req.setAttribute("sv", sv);
             req.setAttribute("rvList", rvList);
+            req.setAttribute("zzim", zzim);
             
             req.getRequestDispatcher("/WEB-INF/views/service/serviceDetailTemp.jsp").forward(req, resp);
         } else {
